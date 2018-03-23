40a5ccee19ed5b9cea3593ea43864c6cb830e0bd
Aibol Kussain <aibolikdev@gmail.com>
2018-03-24 00:20:25 +0300

    wip: Seperating app to multiple routes/views

--- src/client/App.js
+++ src/client/App.js
@@ -12,6 +12,7 @@ import Home from './routes/Home/containers/HomeContainer';
 import Feed from './routes/Feed/containers/FeedContainer';
 import Registration from './routes/Registration/containers/RegistrationContainer';
 import Authentication from './routes/Authentication/containers/AuthenticationContainer';
+import Todo from './routes/Todo';
 
 const App = ({ name }) => {
   const NavigationWithUser = connect(
@@ -27,6 +28,7 @@ const App = ({ name }) => {
           <Route path="/feed" component={Feed} />
           <Route path="/signup" component={Registration} />
           <Route path="/signin" component={Authentication} />
+          <Route path="/todos" component={Todo} />
           <Redirect to="/" />
         </Switch>
       </div>
--- src/client/angular/todo.js
+++ src/client/angular/todo.js
@@ -11,26 +11,74 @@ app.filter('customDate', function() {
     };
 });
 
-// app.config(['$locationProvider', '$routeProvider',
-// function config($locationProvider, $routeProvider) {
-//     $locationProvider.hashPrefix('!');
-//     $routeProvider.
-//       when('/todos', {
-//         template: `<div ng-controller='todoController'>
-//           <h3>This is Todo app</h3>
-//           <ul>
-//             <li ng-repeat='task in tasks'>
-//               {{task.text}}
-//             </li>
-//           </ul>
-//         </div>`,
-//         controller: 'todoController'
-//       })
-//       .otherwise('/', {
-//         template: '<div>Home</div>'
-//       });
-// }]
-// );
+app.config(['$locationProvider', '$routeProvider',
+function config($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
+    $routeProvider.
+      when('/todos/add', {
+        template: `
+          <h3>Adding todo</h3>
+          <form ng-submit="addTodo()" name='form'>
+          <input id="new-todo" ng-model='newTodo' placeholder="What you need to do?">
+          <input type="button" value="Add todo" ng-click="addTodo()" />
+        </form>
+        <div class='loading' ng-class='{"visible": loading}'></div>
+        `,
+        controller: 'addTodoController'
+      })
+      .when('/todos/:todoId/edit', {
+        template: `
+          <h3>Editing todo</h3>
+          <form ng-submit="editTodo()" name='form'>
+          <input id="edit-todo" ng-model='currentTodo.text' placeholder="Loading...">
+          <input type="button" value="Edit todo" ng-click="editTodo()" />
+        </form>
+        <div class='loading' ng-class='{"visible": loading}'></div>
+        `,
+        controller: 'editTodoController'
+      })
+      .when('/todos', {
+        template: `<h3>Todo list</h3>
+        <a href='#!/todos/add' class='add-todo'>Add todo</a>
+        <input ng-model='filteredText' placeholder="Sort by text">
+        <ul>
+          <h4>New todos</h4>
+          <li ng-repeat='task in newTasks | filter:dateFilter | filter:textFilter' ng-click="completeTask(task)">
+            <span class='tasks__item' ng-class='{"tasks__item--done": task.done}'>{{task.text}}</span>
+            <span class='tasks__created-at'>created at {{task.createdAt | customDate}}</span>
+            <a ng-href='#!/todos/{{task._id}}/edit' class='edit-todo'>Edit</a>
+          </li>
+        </ul>
+
+        <ul>
+          <h4>Completed todos</h4>
+          <li ng-repeat='task in completedTasks | filter:dateFilter | filter:textFilter' ng-click="completeTask(task)">
+            <span class='tasks__item' ng-class='{"tasks__item--done": task.done}'>{{task.text}}</span>
+            <span class='tasks__created-at'>created at {{task.createdAt | customDate}}</span>
+            <a ng-href='#!/todos/{{task._id}}/edit' class='edit-todo'>Edit</a>
+          </li>
+        </ul>
+        <ul class='filter'>
+          <li class='filter__item'
+          ng-class='{"filter__item--active": filter === -1}'
+          ng-click="changeFilter(-1)">All</li>
+          <li class='filter__item'
+          ng-class='{"filter__item--active": filter === 0}'
+          ng-click="changeFilter(0)">Created Today</li>
+          <li class='filter__item'
+          ng-class='{"filter__item--active": filter === 1}'
+          ng-click="changeFilter(1)">Created Last day</li>
+          <li class='filter__item'
+          ng-class='{"filter__item--active": filter === 7}'
+          ng-click="changeFilter(7)">Created Last week</li>
+        </ul>`,
+        controller: 'todoListController'
+      })
+      .otherwise('/', {
+        template: '<div>Home</div>'
+      });
+}]
+);
 app.directive('min20symbols', function() {
   return {
     require: 'ngModel',
@@ -49,79 +97,47 @@ app.directive('min20symbols', function() {
   };
 });
 
-app.factory('todoFactory', () => {
-  const tasksList = [
-    {
-      id: 0,
-      text: 'Yesterdays\' task',
-      done: false,
-      createdAt: moment().subtract(1, 'days')
-    },
-    {
-      id: 1,
-      text: 'A week ago',
-      done: true,
-      createdAt: moment().subtract(7, 'days')
-    },
-    {
-      id: 2,
-      text: 'Today\'s task',
-      done: false,
-      createdAt: moment()
-    },
-  ];
-
+app.factory('todoFactory', ['$http', ($http) => {
   return {
     getTasks() {
-      return tasksList;
+      return $http.get('https://sdubot.jsindev.party:1000/todos').then(
+        (res) => {
+          return res.data.items;
+        }
+      );
     },
     getFilteredTasks(minutesDiff) {
-      return tasksList.filter(item => {
+      return this.tasksList.filter(item => {
         return moment().diff(moment.unix(item.createdAt), 'minutes') < minutesDiff;
       });
     },
     getCompletedTasks() {
-      return tasksList.filter(item => item.done);
+      return this.tasksList.filter(item => item.done);
     },
     getNewTasks() {
-      return tasksList.filter(item => !item.done);
+      return this.tasksList.filter(item => !item.done);
     },
     addTodo(todo) {
-      tasksList.push(todo);
+      this.tasksList.push(todo);
     },
     completeTask(task) {
-      let index = tasksList.findIndex((el) => el === task);
-      tasksList[index].done = !tasksList[index].done;
+      let index = this.tasksList.findIndex((el) => el === task);
+      this.tasksList[index].done = !this.tasksList[index].done;
     }
   };
-});
+}]);
 
-app.controller('todoController', ['$scope', 'todoFactory', ($scope, todoFactory) => {
-  $scope.completedTasks = todoFactory.getCompletedTasks();
-  $scope.newTasks = todoFactory.getNewTasks();
-  $scope.newTodo = '';
-  $scope.filter = -1;
-  $scope.textFilter = '';
-
-  $scope.addTodo = () => {
-    console.log($scope.newTodo);
-    let todo = {
-      text: $scope.newTodo,
-      id: uuid.v1(),
-      done: false,
-      createdAt: moment()
-    };
-
-    if(!todo.text) {
-      alert('Text of todo is mandatory...');
-      return;
-    }
-
-    todoFactory.addTodo(todo);
-    $scope.newTodo = '';
+app.controller('todoListController', ['$scope', 'todoFactory', ($scope, todoFactory) => {
+  todoFactory.getTasks().then(items => {
+    todoFactory.tasksList = items;
+    console.log('tasksList', todoFactory.tasksList);
     $scope.completedTasks = todoFactory.getCompletedTasks();
     $scope.newTasks = todoFactory.getNewTasks();
-  };
+    console.log(todoFactory.getCompletedTasks());
+    console.log(todoFactory.getNewTasks());
+  });
+  $scope.filter = -1;
+  $scope.filteredText = '';
 
   $scope.completeTask = (task) => {
     todoFactory.completeTask(task);
@@ -134,13 +150,72 @@ app.controller('todoController', ['$scope', 'todoFactory', ($scope, todoFactory)
     return Math.abs(moment(task.createdAt).diff(moment(), 'days')) <= $scope.filter;
   };
 
-  $scope.dateFilter = task => {
-    if($scope.textFilter.length === 0) return true;
-    return task.text.toLowerCase().indexOf($scope.textFilter.toLowerCase()) > -1;
+  $scope.textFilter = task => {
+    if($scope.filteredText.length === 0) return true;
+    return task.text.toLowerCase().indexOf($scope.filteredText.toLowerCase()) > -1;
   };
 
   $scope.changeFilter = filter => {
     $scope.filter = filter;
   };
 
+}]);
+
+
+app.controller('addTodoController', ['$scope', '$http', '$location', function($scope, $http, $location) {
+  $scope.loading = false;
+  $scope.newTodo = '';
+
+  $scope.addTodo = () => {
+    let todo = {
+      text: $scope.newTodo,
+      done: false,
+      createdAt: moment()
+    };
+
+    if(!todo.text) {
+      alert('Text of todo is mandatory...');
+      return;
+    }
+
+    $scope.loading = true;
+    $http.post('https://sdubot.jsindev.party:1000/todos', todo)
+      .then(res => {
+        $scope.loading = false;
+        $scope.newTodo = '';
+        $location.path('/todos').hash('todos');
+      });
+  };
+}]);
+
+
+app.controller('editTodoController', ['$scope', '$http', '$location', '$routeParams',
+  function($scope, $http, $location, $routeParams) {
+    $scope.loading = true;
+    $scope.currentTodo = {};
+
+    $http.get(`https://sdubot.jsindev.party:1000/todos/${$routeParams.todoId}`)
+      .then(res => {
+        console.log(res);
+        $scope.currentTodo = res.data;
+        $scope.loading = false;
+      });
+      
+    $scope.editTodo = () => {
+      let todo = {
+        text: $scope.currentTodo.text,
+      };
+
+      if(!todo.text) {
+        alert('Text of todo is mandatory...');
+        return;
+      }
+
+      $scope.loading = true;
+      $http.put(`https://sdubot.jsindev.party:1000/todos/${$scope.currentTodo._id}`, todo)
+        .then(res => {
+          $scope.loading = false;
+          $location.path('/todos').hash('todos');
+        });
+    };
 }]);
