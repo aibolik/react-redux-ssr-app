0be67fdc85dec87c792294b13669d862e17ca26a
Aibol Kussain <aibolikdev@gmail.com>
2018-03-25 20:32:57 +0300

    feat: implement articles

--- src/client/angular/todo.js
+++ src/client/angular/todo.js
@@ -3,9 +3,6 @@ import './index.scss';
 import moment from 'moment';
 import uuid from 'uuid';
 const app = angular.module('todoApp', ['ngRoute']);
-import './controllers/todoListController';
-import './controllers/addTodoController';
-import './controllers/editTodoController';
 
 app.filter('customDate', function() {
     return function(d) {
@@ -16,18 +13,18 @@ app.filter('customDate', function() {
 app.config(['$locationProvider', '$routeProvider',
 function config($locationProvider, $routeProvider) {
     $locationProvider.hashPrefix('!');
-    $routeProvider.
-      when('/todos/add', {
-        templateUrl: require('./views/addTodo.html'),
-        controller: 'addTodoController'
+    $routeProvider
+      .when('/articles', {
+        templateUrl: require('./views/articles.html'),
+        controller: 'articleController'
       })
-      .when('/todos/:todoId/edit', {
-        templateUrl: require('./views/editTodo.html'),
-        controller: 'editTodoController'
+      .when('/articles/add', {
+        template: '<article-form action="ADD" article="article" on-add="addArticle(article)"></article-form>',
+        controller: 'articleFormController'
       })
-      .when('/todos', {
-        templateUrl: require('./views/todoList.html'),
-        controller: 'todoListController'
+      .when('/articles/:articleId/edit', {
+        template: '<article-form action="EDIT" article="article" on-edit="editArticle(article)"></article-form>',
+        controller: 'articleFormController'
       })
       .otherwise('/', {
         template: '<div>Home</div>'
@@ -53,37 +50,103 @@ app.directive('min20symbols', function() {
   };
 });
 
-app.factory('todoFactory', ['$http', ($http) => {
+app.service('articleService', ['$http', function($http) {
+  const API_ROOT = 'https://sdubot.jsindev.party:1000/';
+
   return {
-    getTasks() {
-      return $http.get('https://sdubot.jsindev.party:1000/todos').then(
-        (res) => {
-          return res.data.items;
-        }
-      );
-    },
-    getFilteredTasks(minutesDiff) {
-      return this.tasksList.filter(item => {
-        return moment().diff(moment.unix(item.createdAt), 'minutes') < minutesDiff;
-      });
-    },
-    getCompletedTasks() {
-      return this.tasksList.filter(item => item.done);
-    },
-    getNewTasks() {
-      return this.tasksList.filter(item => !item.done);
-    },
-    addTodo(todo) {
-      this.tasksList.push(todo);
-    },
-    completeTask(task) {
-      let index = this.tasksList.findIndex((el) => el === task);
-      this.tasksList[index].loading = true;
-      return $http.put(`https://sdubot.jsindev.party:1000/todos/${this.tasksList[index]._id}`, { done: !this.tasksList[index].done })
+    fetchArticles() {
+      return $http.get(`${API_ROOT}blogs`)
         .then(res => {
-          this.tasksList[index] = res.data;
-          return res;
+          return res.data;
+        });
+    },
+    addArticle(article) {
+      return $http.post(`${API_ROOT}blogs`, article)
+        .then(res => {
+          console.log('Added article', res.data);
+          return res.data;
+        });
+    },
+    editArticle(article) {
+      return $http.put(`${API_ROOT}blogs/${article._id}`, { text: article.text })
+        .then(res => {
+          console.log('Updated article', res.data);
+          return res.data;
         });
     }
   };
 }]);
+
+app.factory('articleFactory', ['articleService', function(articleService) {
+
+  return {
+    articles: [],
+    getArticles() {
+      console.log('getarticles', this.articles);
+      return this.articles.length === 0 ? articleService.fetchArticles().then(articles => {
+        this.articles = articles.items;
+        console.log('getarticles', this.articles);
+        return this.articles;
+      }) : Promise.resolve(this.articles);
+    },
+    getArticleById(articleId) {
+      return this.getArticles().then(res => {
+        return res.find((item) => item._id === articleId);
+      })
+    },
+    addArticle(article) {
+      return articleService.addArticle(article);
+    },
+    editArticle(article) {
+      return articleService.editArticle(article);
+    }
+  };
+}]);
+
+app.controller('articleController', ['$scope', 'articleFactory', function($scope, articleFactory) {
+  console.log('Ctrl init');
+  articleFactory.getArticles().then(articles => {
+    console.log('Articles retrieved', articles);
+    $scope.articles = articles;
+  });
+}]);
+
+app.controller('articleFormController', ['$scope', '$routeParams', 'articleFactory', '$location', function($scope, $routeParams, articleFactory, $location) {
+  var $ctrl = this;
+  let articleId = $routeParams.articleId;
+  $scope.article = '';
+  articleFactory.getArticleById(articleId).then(article => {
+    $scope.article = article;
+  });
+
+  console.log('Article in form', $ctrl.article);
+
+  $scope.addArticle = (article) => {
+    articleFactory.addArticle(article).then(res => {
+      $location.path('articles');
+    });
+  };
+  $scope.editArticle = (article) => {
+    articleFactory.editArticle(article).then(res => {
+      $location.path('articles');
+    })
+  };
+
+}]);
+
+app.component('articleForm', {
+  templateUrl: require('./views/articleForm.html'),
+  bindings: {
+    article: '=',
+    action: '@',
+    onEdit: '&',
+    onAdd: '&'
+  }
+});
+
+app.component('articlesList', {
+  templateUrl: require('./views/articlesList.html'),
+  bindings: {
+    articles: '='
+  }
+});
